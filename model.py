f9c37b157c48d2da895ef4a6ebd9c3b04fb51845
ARUN KUMAR MOHANDAS <arun.mohandas@oracle.com>
2024-07-25 20:57:16 +0530

    Correcting linting issues in model.py file

--- model.py
+++ model.py
@@ -1,12 +1,14 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 
+
 def train_model():
     data = load_iris()
     X, y = data.data, data.target
     model = DecisionTreeClassifier()
     model.fit(X, y)
     return model
+
 
 if __name__ == "__main__":
     model = train_model()
